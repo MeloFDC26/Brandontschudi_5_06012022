7780b5c752bfb7936ebbfb5ccf16950da654a007
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-01-17 16:47:31 +0100

    page d'acceuil mise en place script.js (afichage des canapés)

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,7 @@
 console.log("Acceuil");
-getProducts()
-    .then(function (products) {
-        displayProducts(products);  
-    }
-);
+getProducts().then(function (products) {
+  displayProducts(products);
+});
 
 //Je dois interroger le serveur pour récupérer la liste des produits
 function getProducts() {
@@ -16,11 +14,37 @@ function getProducts() {
     });
 }
 
-//Je dois afficher la liste des produits reçue
+//Je dois afficher la liste des produits reçus
 function displayProducts(products) {
-  console.log(products);
-  for (let product of products) {
+  console.table(products);
+  for (let product in products) {
     const itemsSection = document.getElementById("items");
-    itemsSection.innerText += product.name;
+
+    //Création "élément <a>"
+    let productLink = document.createElement("a");
+    itemsSection.appendChild(productLink);
+    productLink.href = `product.html?id=${products[product]._id}`;
+
+    //Création "élément <article>"
+    let productArticle = document.createElement("article");
+    productLink.appendChild(productArticle);
+
+    // Insertion de l'image
+    let productImg = document.createElement("img");
+    productArticle.appendChild(productImg);
+    productImg.src = products[product].imageUrl;
+    productImg.alt = products[product].altTxt;
+
+    // Insertion du titre "h3"
+    let productName = document.createElement("h3");
+    productArticle.appendChild(productName);
+    productName.classList.add("productName");
+    productName.innerHTML = products[product].name;
+
+    // Insertion de la description "p"
+    let productDescription = document.createElement("p");
+    productArticle.appendChild(productDescription);
+    productDescription.classList.add("productName");
+    productDescription.innerHTML = products[product].description;
   }
 }
