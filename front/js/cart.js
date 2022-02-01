0da4e88885a6c0bca23b4623567d5b3fdd65638e
MeloFDC26 <87497742+MeloFDC26@users.noreply.github.com>
2022-02-01 15:24:42 +0100

    Totaux panier ET modification quantité et prix ET suppression d'un article dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,6 +10,7 @@ function getCart(){
         const emptyCart = `<p>Le panier est vide</p>`;
         const positionEmptyCart = document.getElementById("cart__items");
         positionEmptyCart.innerHTML = emptyCart;
+        console.log(emptyCart);
     }
 // Si au moins un article est dans le panier
     else{
@@ -97,15 +98,75 @@ getCart();
 // Récupération des valeurs totales (quantité et prix)
 function getTotals(){
 
-    // Total quantité
-    let articleQte = document.getElementsByClassName("itemQuantity");  //Valeur de l'input
+    // Total quantité (i est une variable locale)
+    let articleQte = document.getElementsByClassName("itemQuantity");  //On récupère l'input
     let articleQteLength = articleQte.length; //Nombre d'articles dans le [local storage]
-    quantiteTotal = 0; //On initialise 'quantiteTotal' sur 0 pour l'addition
+    quantityTotal = 0; //On initialise 'quantityTotal' sur 0 pour l'addition
     for (let i = 0; i < articleQteLength; ++i) { //Boucle pour additionner le nombre d'articles dans [local storage]
-        quantiteTotal += articleQte[i].valueAsNumber; //Quantité totale (=0) + nombre d'articles = Quantité totale
+        quantityTotal += articleQte[i].valueAsNumber; //Quantité totale (=0) + nombre d'articles = Quantité totale
     }
     let productCartTotalQuantity = document.getElementById('totalQuantity'); //On récupère l'élément '#totalQuantity' dans le HTML
-    productCartTotalQuantity.innerText = quantiteTotal; //Et on modifie le texte dans #totalQuantity par le résultat de l'addition
-    console.log(quantiteTotal);
+    productCartTotalQuantity.innerText = quantityTotal; //Et on modifie le texte dans #totalQuantity par le résultat de l'addition
+    console.log(quantityTotal);
+
+    // Prix total du panier
+    totalPrice = 0; //On initialise 'totalPrice' sur 0 pour les additions
+    for (let i = 0; i < articleQteLength; ++i) { //Boucle pour additionner les prix en faisant la multiplication pour les articles identiques (même id + même couleur)
+        totalPrice += (articleQte[i].valueAsNumber * produitLocalStorage[i].price); //Prix total (=0) + (nombre d'articles * le prix de l'article) = Prix total
+    }
+    let productCartTotalPrice = document.getElementById('totalPrice'); //On récupère l'id #totalPrice
+    productCartTotalPrice.innerText = totalPrice; //Et on modifie le contenu de #totalPrice par le résultat de l'addition
+    console.log(totalPrice);
 }
 getTotals();
+
+// Modification de la quantité d'un article dans le panier (r est une variable locale)
+function modifQuantity(){
+    let inputQuantity = document.querySelectorAll(".itemQuantity"); //On récupère la classe .itemQuantity 
+
+    // Boucle pour changer la quantité et modifier le prix
+    for (let r = 0; r < inputQuantity.length; r++){ //(r++ = rendu avant incrémentation, ++r rend après incrémentation)
+        inputQuantity[r].addEventListener("change", (event) => { //On écoute le changement de valeur dans l'input
+            event.preventDefault();
+
+            // On récupère l'élément à modifier dans [local storage]
+            let quantityModif = produitLocalStorage[r].quantiteProduit; // La quantité 
+            let qttModifValue = inputQuantity[r].valueAsNumber; // La valeur de l'input 
+            const resultFind = produitLocalStorage.find((el) => el.qttModifValue !== quantityModif); // On vérifie que qttModifValue est strictement inégale à quantityModif
+
+            resultFind.quantiteProduit = qttModifValue; //On modifie la quantité initiale par la valeur modifiée
+            produitLocalStorage[r].quantiteProduit = resultFind.quantiteProduit; // On modifie la quantité dans le [local storage]
+
+            localStorage.setItem("produit", JSON.stringify(produitLocalStorage)); //On met à jour le [local storage]
+            console.log(quantityModif);
+        
+            // refresh rapide pour afficher instantanément le résultat (quantité et prix totaux)
+            location.reload();
+        })
+    }
+};
+modifQuantity();  //Grâce à la fonction getTotals() et le refresh rapide, le calcul du prix total est actualisé directement
+
+// Suppression d'un produit du panier (d est une variable locale)
+function productCartDelete(){
+    let btnSupprimerPanier = document.getElementsByClassName("deleteItem"); //On récupère .deleteItem qui représente le bouton 'supprimer'
+    
+    //Boucle pour supprimer l'article au 'click' sur le bouton 'supprimer'
+    for (let d = 0; d < btnSupprimerPanier.length; d++){
+        btnSupprimerPanier[d].addEventListener('click', (event) => {
+            event.preventDefault();
+
+            //On sélectionne les éléments à supprimer dans [local storage] par rapport à l'id et la couleur du produit à supprimer
+            let supprId = produitLocalStorage[d]._id; //L'id du produit dans [local storage]
+            let supprColor = produitLocalStorage[d].couleurProduit; //La couleur du produit dans [local storage]
+            produitLocalStorage = produitLocalStorage.filter(el => el._id !== supprId || el.couleurProduit !== supprColor); //On filtre les id et les couleurs à supprimer (ici on dit :"filtrer 'supprId' et 'supprColor' au click sur le bouton 'supprimer'")
+
+            localStorage.setItem("produit", JSON.stringify(produitLocalStorage)); //On met à jour le [local storage]
+
+            //Alerte produit supprimé et refresh rapide
+            alert("Ce produit a bien été supprimé du panier");
+            location.reload();
+        });
+    }
+}
+productCartDelete();
